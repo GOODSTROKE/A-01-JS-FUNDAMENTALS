202484964f0cd16f6e7e1fea673db679752b4d90
Alley Harmon <112970302+GOODSTROKE@users.noreply.github.com>
2023-06-01 16:56:01 -0400

    Update PracticeQuestions.js
    Finalized

--- PracticeQuestions.js
+++ PracticeQuestions.js
@@ -131,6 +131,42 @@ console.log(pizzaLovers);
 
 //// 7. Show an an example of a switch statement being used
 
+const dayOfWeek = 3;
+let dayName;
+
+switch (dayOfWeek) {
+  case 1:
+    dayName = 'Sunday';
+    break;
+  case 2:
+    dayName = 'Monday';
+    break;
+  case 3:
+    dayName = 'Tuesday';
+    break;
+  case 4:
+    dayName = 'Wednesday';
+    break;
+  case 5:
+    dayName = 'Thursday';
+    break;
+  case 6:
+    dayName = 'Friday';
+    break;
+  case 7:
+    dayName = 'Saturday';
+    break;
+  default:
+    dayName = 'Invalid day';
+    break;
+}
+
+console.log(dayName);
+
+// In this example, we use a switch statement to assign a day name to the variable dayName based on the dayOfWeek variable.
+// The switch statement evaluates the value of dayOfWeek and matches it to a specific case. In each case, 
+// we assign the corresponding day name to dayName. If none of the cases match, the default case is executed, and 
+// dayName is set to 'Invalid day'. Finally, we log the value of dayName to the console.
 
 
 
@@ -150,7 +186,7 @@ const userPersonalData = {
  };
   
 
-// 8. Combine the personalData and userGameData into a user object that is equal to the object below, by using the spread operator:
+//// 8. Combine the personalData and userGameData into a user object that is equal to the object below, by using the spread operator:
 // const user = {
 //  name: 'peter',
 //  age: '56',
@@ -159,16 +195,29 @@ const userPersonalData = {
 //  accomplishments: ['won award for being good gamer', 'won 1st win', 'got good score on the weekend'],
 // }
 
+const user = {
+  ...userPersonalData,
+  ...userGameData,
+};
+
+console.log(user);
 
 
+//// 9. Make a copy of your new user object but override the birthday to december 31st
 
+const newUser = {
+  ...user,
+  birthday: 'December 31st',
+};
 
-// 9. Make a copy of your new user object but override the birthday to december 31st
+console.log(newUser);
  
 
-// 10. Use the spread operator to make a copy of the accomplishments array and store it in a new variable
+//// 10. Use the spread operator to make a copy of the accomplishments array and store it in a new variable
 
+const copiedAccomplishments = [...user.accomplishments];
 
+console.log(copiedAccomplishments);
 
 
 //  11.Given the object bellow, use object destructuring to get the favorite food value (user.name.favoriteThings.food)
@@ -183,14 +232,32 @@ var user = {
   },
  };
 
- 
-// 12. Once you have grabbed the favorite foods. Destructure the food array to grab only the first 2 values. //
 
 
-// 13. use object destructuring and the rest operator to transform the following array into 3 variables: name, age, and food. 
-//    the food variable should have all the array items starting from the third one.
+ const { favoriteThings: { food } } = user;
+
+console.log(food);
+
+//// 12. Once you have grabbed the favorite foods. Destructure the food array to grab only the first 2 values. //
+
+const [food1, food2] = food;
+
+console.log(food1, food2);
+
+
+//// 13. use object destructuring and the rest operator to transform the following array into 3 variables: name, age, and food. 
+//// the food variable should have all the array items starting from the third one.
 const data = ['peter', '34', 'apple', 'oranges', 'pizza', 'tacos'];
 
+const {
+  name: userName,
+  favorites: {
+    needs: { food: favoriteFood },
+    wants: { things: [favoriteThing, secondfavoriteThing] },
+  },
+} = userInfo;
+
+console.log(userName, favoriteFood, favoriteThing, secondfavoriteThing);
 
 
 // 14. use object destructuring to grab the following from the userInfo object: 
@@ -198,6 +265,7 @@ const data = ['peter', '34', 'apple', 'oranges', 'pizza', 'tacos'];
 // - The user's favorite food array and name it favoriteFood.
 // - The users first favorite thing (cars) and name it favoriteThing
 // - The users second favorite thing (jewelry) and name it secondfavoriteThing
+
 
 const userInfo = {
   name: 'Peter',
@@ -255,7 +323,27 @@ var fetchData = () => new Promise((resolve, reject) => {
 // 15. Call fetchData (which returns a promise) and use the .then()  method to log the value the promise resolves with to the javascript console.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+fetchData()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 16. Call fetchData (which returns a promise) and use the async/await method to log the value the promise resolves with to the javascript console.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function fetchDataAsync() {
+  try {
+    const result = await fetchData();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchDataAsync();
+
